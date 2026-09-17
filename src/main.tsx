8278ba7d39bf668c613d175872e0efb0f7b4e5f4
MD. ABDUR RAHMAN HRIDOY <alamgirhossainbogura@gmail.com>
2026-09-17 16:16:09 +0600

    Add conditional rendering for GuardianView component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import GuardianView from './GuardianView.tsx'
 import './index.css'
+
+const isGuardianView = new URLSearchParams(window.location.search).get('guardian') === '1';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {isGuardianView ? <GuardianView /> : <App />}
   </React.StrictMode>,
 )
